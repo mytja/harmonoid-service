37ec9cb244c0e18d36c3d60ab49ef6d5fb47d325
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-08-05 18:30:38 +0530

    🎉 Added /search, /searchyoutube, /albuminfo, /trackinfo, /artistrelated & /trackdownload

---
+++ harmonoidservice/__init__.py
@@ -0,0 +1,9 @@
+from harmonoidservice.spotify import SpotifyHandler
+from harmonoidservice.youtube import YoutubeHandler
+
+
+class HarmonoidService(SpotifyHandler, YoutubeHandler):
+
+    def __init__(self, clientId, clientSecret):
+        self.clientId = clientId
+        self.clientSecret = clientSecret
---
+++ harmonoidservice/youtube.py
@@ -0,0 +1,70 @@
+from youtubesearchpython import SearchVideos
+import youtube_dl
+import json
+from urllib.request import urlopen
+from urllib.request import Request
+import os
+from flask import make_response
+
+
+class YoutubeHandler:
+
+    def AudioUrl(self, videoId):
+        import youtube_dl
+        ydl_opts = {
+            'format': '140',
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(
+                f'https://www.youtube.com/watch?v={videoId}', download=False)
+            return info['formats'][0]['url']
+
+    def SearchYoutube(self, keyword, offset, mode, maxResults):
+        if keyword != None:
+            search = SearchVideos(keyword, offset, mode, maxResults)
+            response =  make_response(search.result(), 200)
+            response.headers.set('Content-Type', 'application/json')
+            return response
+        else:
+            return make_response('bad request', 400)
+
+    def TrackDownload(self, trackId, trackName):
+
+        if trackId != None:
+            trackInfo = json.loads(self.TrackInfo(trackId))        
+            artists = ''
+            for artist in trackInfo['album_artists']:
+                artists+=artist+' '
+
+            videoId = self.SearchYoutube(trackInfo['track_name'] + ' ' + artists + ' ' + trackInfo['album_name'], 1, 'dict', 1)['search_result'][0]['id']
+            audioUrl = self.AudioUrl(videoId)
+
+            audioRequest = Request(
+                url = audioUrl,
+                data = None
+            )
+            audioResponse = urlopen(audioRequest)
+            audioBytes = audioResponse.read()
+            print()
+            response = make_response(audioBytes, 200)
+            response.headers.set('Content-Type', 'audio/mp4')
+            response.headers.set('Content-Length', audioResponse.headers['Content-Length'])
+
+            return response
+        elif trackName != None:
+            videoId = self.SearchYoutube(trackName, 1, 'dict', 1)['search_result'][0]['id']
+            audioUrl = self.AudioUrl(videoId)
+
+            audioRequest = Request(
+                url = audioUrl,
+                data = None
+            )
+            audioResponse = urlopen(audioRequest)
+            audioBytes = audioResponse.read()
+            response = make_response(audioBytes, 200)
+            response.headers.set('Content-Type', 'audio/mp4')
+
+            return response
+        else:
+            return make_response('bad request', 400)
