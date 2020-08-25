3c30a6c901e7fc020c158dce960ef49060c1455d
Denis <raitonoberu@mail.ru>
2020-08-25 19:53:21 +0700

    Create async_youtube_dl.py
    Ok, dat works.

---
+++ harmonoidservice/async_youtube_dl.py
@@ -0,0 +1,31 @@
+import subprocess
+import asyncio
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+COMMAND = 'youtube-dl --format "{format}" --no-progress --no-playlist -x -o "{output}" "{url}"'
+# TODO: use aria2c to increase speed
+
+async def download(url, output, format="140"):
+    logger.info("[youtube-dl] Downloading video " + url)
+    cmd = COMMAND.format(url=url, output=output, format=format)
+    process = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    while process.poll() is None:
+        await asyncio.sleep(0.1)
+
+    stdout, stderr = process.communicate()
+    stdout, stderr = stdout.decode(), stderr.decode()
+
+    if process.poll() == 0:
+        # success
+        logger.info(stdout)
+        return True
+    else:
+        # error
+        logger.error(stderr)
+        return False
