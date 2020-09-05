f21d942cc89bede263dc7dcf13a7b666d64db85b
Denis <raitonoberu@mail.ru>
2020-09-05 20:19:10 +0700

    Working downloading

--- harmonoidservice/downloading.py
+++ harmonoidservice/downloading.py
@@ -11,36 +11,41 @@ logger = logging.getLogger(__name__)
 
 
 class DownloadHandler:
-    async def SaveAudio(self, videoId, trackId):
+    async def SaveAudio(self, trackId):
         result = await async_youtube_dl.download(
-            f"https://www.youtube.com/watch?v={videoId}", f"{trackId}.m4a"
+            f"https://www.youtube.com/watch?v={trackId}", f"{trackId}.m4a"
         )
         if result:
             logger.info(
                 f"[download] Track download successful for track ID: {trackId}."
             )
         else:
-            raise HTTPException(500, "Couldn't download video " + videoId)
+            raise HTTPException(500, "Couldn't download video " + trackId)
 
     async def SaveMetaData(self, trackInfoJSON):
-        logger.info("[metadata] Getting album art: " + trackInfoJSON["album_art_640"])
-
-        async with httpx.AsyncClient() as client:
-            response = await client.get(trackInfoJSON["album_art_640"])
-
-        albumArtBinary = response.content
-
-        logger.info("[metadata] Album art retrieved.")
+        art = (
+            trackInfoJSON["album_art_640"]
+            or trackInfoJSON["album_art_300"]
+            or trackInfoJSON["album_art_64"]
+        )
 
         trackId = trackInfoJSON["track_id"]
 
         audioFile = MP4(f"{trackId}.m4a")
         await audioFile.init()
 
-        cover = MP4Cover(albumArtBinary, imageformat=MP4Cover.FORMAT_JPEG)
-        await cover.init()
+        if art:
+            logger.info("[metadata] Getting album art: " + art)
+            async with httpx.AsyncClient() as client:
+                response = await client.get(art)
+            albumArtBinary = response.content
+            logger.info("[metadata] Album art retrieved.")
+            cover = MP4Cover(albumArtBinary, imageformat=MP4Cover.FORMAT_JPEG)
+            await cover.init()
+            audioFile["covr"] = [cover]
+        else:
+            logger.info("[metadata] Album art is not found.")
 
-        audioFile["covr"] = [cover]
         audioFile["\xa9nam"] = trackInfoJSON["track_name"]
         audioFile["\xa9alb"] = trackInfoJSON["album_name"]
         audioFile["\xa9ART"] = "/".join(trackInfoJSON["track_artists"])
@@ -50,7 +55,7 @@ class DownloadHandler:
             (trackInfoJSON["track_number"], trackInfoJSON["album_length"])
         ]
         audioFile["\xa9cmt"] = (
-            "https://open.spotify.com/track/" + trackInfoJSON["track_id"]
+            "https://music.youtube.com/watch?v=" + trackInfoJSON["track_id"]
         )
         await audioFile._save()
 
@@ -58,49 +63,25 @@ class DownloadHandler:
 
     async def TrackDownload(self, trackId, trackName):
         if trackId:
-            if os.path.isfile(f"{trackId}.m4a"):
-                return FileResponse(
-                    f"{trackId}.m4a",
-                    media_type="audio/mp4",
-                    headers={"Accept-Ranges": "bytes"},
-                )
-
             logger.info(f"[server] Download request in ID format.")
-            trackInfo = await self.TrackInfo(trackId)
-            logger.info(
-                f"[info] Successfully retrieved metadata of track ID: {trackId}."
-            )
-            artists = " ".join(trackInfo["track_artists"])
-
-            videoId = await self.ytMusic._search(trackInfo["track_name"], "songs")
-            videoId = videoId[0]["videoId"]
-
-            logger.info(f"[search] Search successful. Video ID: {videoId}.")
         if trackName:
             logger.info(f"[server] Download request in name format.")
-            videoId = await self.ytMusic._search(trackInfo["track_name"], "songs")
-            videoId = videoId[0]["videoId"]
-            logger.info(f"[search] Search successful. Video ID: {videoId}.")
-            trackId = await self.SearchSpotify(trackName, "track", 0, 1)
-            trackId = trackId["tracks"][0]["track_id"]
-            logger.info(f"[tracksearch] Track Search successful. Track ID: {trackId}.")
+            trackId = await self.ytMusic._search(trackInfo["track_name"], "songs")
+            trackId = trackId[0]["videoId"]
 
-            if os.path.isfile(f"{trackId}.m4a"):
-                return FileResponse(
-                    f"{trackId}.m4a",
-                    media_type="audio/mp4",
-                    headers={"Accept-Ranges": "bytes"},
-                )
-
-            trackInfo = await self.TrackInfo(trackId)
-            logger.info(
-                f"[info] Successfully retrieved metadata of track ID: {trackId}."
+        if os.path.isfile(f"{trackId}.m4a"):
+            return FileResponse(
+                f"{trackId}.m4a",
+                media_type="audio/mp4",
+                headers={"Accept-Ranges": "bytes"},
             )
 
-        await self.SaveAudio(videoId, trackId)
+        trackInfo = await self.TrackInfo(trackId)
+        logger.info(f"[info] Successfully retrieved metadata of track ID: {trackId}.")
+        await self.SaveAudio(trackId)
         await self.SaveMetaData(trackInfo)
-        logger.info(f"[server] Sending audio binary for track ID: {trackId}")
 
+        logger.info(f"[server] Sending audio binary for track ID: {trackId}")
         return FileResponse(
             f"{trackId}.m4a",
             media_type="audio/mp4",
