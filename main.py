3f789832b8c8681608eb75604e72504d0f57cbd9
mytja <52399966+mytja@users.noreply.github.com>
2020-12-14 13:17:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -66,22 +66,22 @@ async def TrackDownload(track_id=None, album_id=None, track_name=None):
     return await harmonoidService.TrackDownload(track_id, album_id, track_name)
 
 @app.get("/test")
-async def Test():
+def Test():
     import time
     
-    response = await SearchYoutube("NCS", "track")
+    response = SearchYoutube("NCS", "track")
     if (response != None):
         __musicsearchtest = "OK!"
     else:
         __musicsearchtest = "Fail!"
     
-    response = await SearchYoutube("NCS", "album")
+    response = SearchYoutube("NCS", "album")
     if (response != None):
         __albumsearchtest = "OK!"
     else:
         __albumsearchtest = "Fail!"
     
-    response = await SearchYoutube("NCS", "artist")
+    response = SearchYoutube("NCS", "artist")
     if (response != None):
         __artistsearchtest = "OK!"
     else:
