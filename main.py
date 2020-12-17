977606087e54ecd9bf0cf4a6548edc5f9585c654
mytja <52399966+mytja@users.noreply.github.com>
2020-12-17 08:53:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -72,6 +72,7 @@ async def Test():
     import time
     
     response = await SearchYoutube("NCS", "track")
+    response = response.text
     response = json.dumps(response, indent=4)
     ifin =  "track_id" in response
     if (response != None and ifin==True):
