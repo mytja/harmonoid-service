09c3fe052e4c422d7c7ff86c6982cb724090e28d
mytja <52399966+mytja@users.noreply.github.com>
2020-12-22 11:01:09 +0100

    Pytubetest (#23)
    * Added few features to improve quality on some songs
    
    * Added only audio filter
    
    * Update downloading.py
    
    * Update requirements.txt
    
    * Update downloading.py
    
    * Update downloading.py
    
    * Update downloading.py
    
    * Added a few new features
    
    * Updated README to remove ytdl
    
    * 👌 Cleaning up unnecessary code and added speed of test
    
    * 👏 Added speed log
    
    Now we can know how fast it downloaded it!
    
    * Update downloading.py
    
    * 🤔 I was dumb when I wrote this, i guess...

--- harmonoidservice/downloading.py
+++ harmonoidservice/downloading.py
@@ -6,9 +6,9 @@ import os
 import sys
 import asyncio
 from .async_mutagen import MP3
-import youtube_dl
 from pytube import *
 import ytmusicapi
+import time
 
 #CURRENT_VERSION = __version__  # just to avoid reimports
 MUSICAPI_VERSION = ytmusicapi.__version__
@@ -48,38 +48,6 @@ class DownloadHandler:
                 print("[stderr]", stderr)
         else:
             print("[update] ytmusicapi is already updated.")
-    """
-    async def UpdatePyTube(self):
-        async with httpx.AsyncClient() as client:
-            latestVersion = await client.get("https://api.github.com/repos/nficano/pytube/releases")
-        latestVersion = latestVersion.text[0]["tag_name"]
-
-        global CURRENT_VERSION
-        updated = latestVersion == CURRENT_VERSION
-        print(f"[update] Installed PyTube version  : {CURRENT_VERSION}.")
-        print(f"[update] Latest PyTube Version     : {latestVersion}.")
-        if not updated:
-            print("[update] Updating PyTube...")
-            cmd = f"{sys.executable} -m pip install --upgrade pytube"
-
-            process = subprocess.Popen(
-                cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-            )
-            while process.poll() is None:
-                await asyncio.sleep(0.1)
-            stdout, stderr = process.communicate()
-            stdout, stderr = stdout.decode(), stderr.decode()
-
-            if process.poll() == 0:
-                CURRENT_VERSION = latestVersion
-                print(f"[update] Updated To PyTube version : {latestVersion}")
-            else:
-                print("[update] Failed to update.")
-                print("[stdout]", stdout)
-                print("[stderr]", stderr)
-        else:
-            print("[update] PyTube is already updated.")
-    """
     async def SaveAudio(self, trackId):
         # Download
         yt = YouTube('https://youtube.com/watch?v='+trackId)
@@ -126,6 +94,8 @@ class DownloadHandler:
         print(f"[metadata] Successfully added metadata to track ID: {trackId}.")
 
     async def TrackDownload(self, trackId, albumId, trackName, retry=True):
+        start_time = time.time()
+        
         if trackId:
             print(f"[server] Download request in ID format.")
         if trackName:
@@ -137,6 +107,7 @@ class DownloadHandler:
             print(
                 f"[youtube] Track already downloaded for track ID: {trackId}.\n[server] Sending audio binary for track ID: {trackId}."
             )
+            print("[speed] Returning already downloaded file took %s seconds" % (time.time() - start_time))
             return FileResponse(
                 f"{trackId}.mp3",
                 media_type="audio/mpeg",
@@ -152,6 +123,7 @@ class DownloadHandler:
                 await self.SaveMetaData(trackInfo)
 
                 print(f"[server] Sending audio binary for track ID: {trackId}")
+                print("[speed] Downloading, adding metadata and returning took %s seconds" % (time.time() - start_time))
                 return FileResponse(
                     f"{trackId}.mp3",
                     media_type="audio/mpeg",
@@ -170,6 +142,7 @@ class DownloadHandler:
                      return updatedResponse
                  print("\n[diagnosis] Diagnosis finished without success!")
                  print(f"[server] Sending status code 500 for track ID: {trackId}.")
+                 print("[speed] Fail with diagnosis took %s seconds" % (time.time() - start_time))
                  return PlainTextResponse(
                     content=f"Internal Server Error.\nYouTube-DL Failed.\n{str(error)}",
                     status_code=500,
@@ -177,6 +150,7 @@ class DownloadHandler:
         else:
             print(f"[youtube] Could not retrieve metadata of track ID: {trackId}.")
             print(f"[server] Sending status code 500 for track ID: {trackId}.")
+            print("[speed] Fail took %s seconds" % (time.time() - start_time))
             return PlainTextResponse(
                 content=trackInfo,
                 status_code=500,
