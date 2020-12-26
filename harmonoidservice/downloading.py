0912d5fa412a225fe007b468e795f69cb78ef93e
Denis <raitonoberu@mail.ru>
2020-12-26 20:58:58 +0700

    Improve downloading

--- harmonoidservice/downloading.py
+++ harmonoidservice/downloading.py
@@ -1,33 +1,34 @@
 import httpx
 from fastapi.responses import FileResponse, PlainTextResponse
 import subprocess
+import aiofiles
 import aiofiles.os
 import os
 import sys
 import asyncio
 from .async_mutagen import MP3
-from pytube import *
+from pytube import YouTube
 import ytmusicapi
 import time
 
-#CURRENT_VERSION = __version__  # just to avoid reimports
 MUSICAPI_VERSION = ytmusicapi.__version__
 
 
 class DownloadHandler:
     async def UpdateYTMusicAPI(self):
         async with httpx.AsyncClient() as client:
-            latestVersion = await client.get("https://api.github.com/repos/sigma67/ytmusicapi/release")
-        latestVersion = latestVersion.text
-        
-        jsonLoad = json.loads(latestVersion)[0]["tag_name"]
-        
-        updated = (jsonLoad == MUSICAPI_VERSION)
+            latestVersion = await client.get(
+                "https://api.github.com/repos/sigma67/ytmusicapi/release"
+            )
+        latestVersion = latestVersion.json()[0]["tag_name"]
+
+        global MUSICAPI_VERSION
+        updated = latestVersion == MUSICAPI_VERSION
         print(f"[update] Installed ytmusicapi version  : {MUSICAPI_VERSION}.")
-        print(f"[update] Latest ytmusicapi Version     : {jsonLoad}.")
+        print(f"[update] Latest ytmusicapi Version     : {latestVersion}.")
         if not updated:
             print("[update] Updating ytmusicapi...")
-            cmd = f"{sys.executable} -m pip install --upgrade ytmusicapi"
+            cmd = f"{sys.executable} -m pip install --upgrade git+https://github.com/sigma67/ytmusicapi@master"
 
             process = subprocess.Popen(
                 cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
@@ -38,46 +39,65 @@ class DownloadHandler:
             stdout, stderr = stdout.decode(), stderr.decode()
 
             if process.poll() == 0:
-                MUSICAPI_VERSION = jsonLoad
-                print(
-                    f"[update] Updated To ytmusicapi version : {latestVersion}"
-                )
+                MUSICAPI_VERSION = latestVersion
+                print(f"[update] Updated To ytmusicapi version : {latestVersion}")
             else:
                 print("[update] Failed to update.")
                 print("[stdout]", stdout)
                 print("[stderr]", stderr)
         else:
             print("[update] ytmusicapi is already updated.")
-    async def SaveAudio(self, trackId):
-        # Download
-        yt = YouTube('https://youtube.com/watch?v='+trackId)
+
+    def _getUrl(self, trackId):
+        # (url, extension)
+        yt = YouTube("https://youtube.com/watch?v=" + trackId)
         yt_streams = yt.streams.filter(only_audio=True).order_by("abr").desc()
-        print("[pytube] YT streams avaiable")
-        print(yt_streams)
-        yt_streams = yt_streams.first().download()
-        
-        yt_end = yt_streams.split(".")
-        print(yt_end)
-        yt_list_len = len(yt_end)
-        if (yt_end[yt_list_len-1] != "mp3"):
+        print(f"[pytube] {len(yt_streams)} YT streams avaiable")
+        stream = yt_streams.first()
+        if stream:
+            return (stream.url, stream.default_filename.split(".")[-1])
+        return (None, None)
+
+    async def SaveAudio(self, trackId):
+        # (status, reason)
+        loop = asyncio.get_running_loop()
+        url, ext = await loop.run_in_executor(None, self._getUrl, trackId)
+
+        if not url:
+            return (False, "Stream not found")
+
+        filename = f"{trackId}.{ext}"
+
+        print("[downloading] Downloading to " + filename)
+        async with httpx.AsyncClient() as client:
+            # simple, but eats RAM:
+            # async with aiofiles.open(filename, "wb") as file:
+            #     await file.write(await client.get(url))
+            async with client.stream("GET", url) as response:
+                async with aiofiles.open(filename, "wb") as file:
+                    async for chunk in response.aiter_bytes():
+                        await file.write(chunk)
+
+        if ext != "mp3":
             print("[conversion] Stream isn't mp3. Converting")
-            # Convert
-            cmd = 'ffmpeg -i "'+yt_streams+'" '+trackId+".mp3"
-            print("[conversion] CMD line: "+cmd)
-            os.system(cmd)
-        else:
-            print("[system] Moving MP3 file to output folder")
-            cmd = 'mv "'+yt_streams+'" '+trackId+".mp3"
-            os.system(cmd)
-        
-        #Success!
+            cmd = 'ffmpeg -i "' + filename + '" ' + trackId + ".mp3"
+
+            process = subprocess.Popen(
+                cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+            while process.poll() is None:
+                await asyncio.sleep(0.1)
+            stdout, stderr = process.communicate()
+            stdout, stderr = stdout.decode(), stderr.decode()
+            if process.poll() != 0:
+                print("[stdout]", stdout)
+                print("[stderr]", stderr)
+                return (False, "Couldn't convert")
+
+            asyncio.create_task(aiofiles.os.remove(filename))
+
         print(f"[youtube] Track download successful for track ID: {trackId}.")
         return (True, None)
-        #RP
-        #except:
-            #print(f"[youtube] Track download unsuccessful for track ID: {trackId}.")
-            #print(f"[metadata] Skipped adding metadata to track ID: {trackId}.")
-            #return (False, None)
 
     async def SaveMetaData(self, trackInfoJSON):
         art = (
@@ -93,9 +113,9 @@ class DownloadHandler:
 
         print(f"[metadata] Successfully added metadata to track ID: {trackId}.")
 
-    async def TrackDownload(self, trackId, albumId, trackName, retry=True):
+    async def TrackDownload(self, trackId, albumId, trackName):
         start_time = time.time()
-        
+
         if trackId:
             print(f"[server] Download request in ID format.")
         if trackName:
@@ -107,7 +127,10 @@ class DownloadHandler:
             print(
                 f"[youtube] Track already downloaded for track ID: {trackId}.\n[server] Sending audio binary for track ID: {trackId}."
             )
-            print("[speed] Returning already downloaded file took %s seconds" % (time.time() - start_time))
+            print(
+                "[speed] Returning already downloaded file took %s seconds"
+                % (time.time() - start_time)
+            )
             return FileResponse(
                 f"{trackId}.mp3",
                 media_type="audio/mpeg",
@@ -118,35 +141,25 @@ class DownloadHandler:
         if type(trackInfo) == dict:
             print(f"[youtube] Successfully retrieved metadata of track ID: {trackId}.")
 
-            status, error = await self.SaveAudio(trackId)
+            status, reason = await self.SaveAudio(trackId)
             if status:
                 await self.SaveMetaData(trackInfo)
 
                 print(f"[server] Sending audio binary for track ID: {trackId}")
-                print("[speed] Downloading, adding metadata and returning took %s seconds" % (time.time() - start_time))
+                print(
+                    "[speed] Downloading, adding metadata and returning took %s seconds"
+                    % (time.time() - start_time)
+                )
                 return FileResponse(
                     f"{trackId}.mp3",
                     media_type="audio/mpeg",
                     headers={"Accept-Ranges": "bytes"},
                 )
             else:
-                 if retry:
-                     print("\n[diagnosis] (1/2) Deleting cookies file.")
-                     await aiofiles.os.remove("cookies.txt")
-                     print("\n[diagnosis] (2/2) Updating YTMusicAPI.")
-                     await UpdateYTMusicAPI()
-                     print(f"[diagnosis] Retrying download for track ID: {trackId}.\n")
-                     updatedResponse = await self.TrackDownload(
-                         trackId, albumId, trackName, retry=False
-                     )
-                     return updatedResponse
-                 print("\n[diagnosis] Diagnosis finished without success!")
-                 print(f"[server] Sending status code 500 for track ID: {trackId}.")
-                 print("[speed] Fail with diagnosis took %s seconds" % (time.time() - start_time))
-                 return PlainTextResponse(
-                    content=f"Internal Server Error.\nYouTube-DL Failed.\n{str(error)}",
+                return PlainTextResponse(
+                    content=reason,
                     status_code=500,
-                    )
+                )
         else:
             print(f"[youtube] Could not retrieve metadata of track ID: {trackId}.")
             print(f"[server] Sending status code 500 for track ID: {trackId}.")
